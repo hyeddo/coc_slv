a3055874ee5dc2ba4386d8b1002b0332030e771d
임재현 <ljh040513@naver.com>
2023-11-18 21:33:29 +0900

    [ADD] qst 새 프로젝트 생성

---
+++ QstApplication.java
@@ -0,0 +1,13 @@
+package com.slv.qst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QstApplication.class, args);
+    }
+
+}
